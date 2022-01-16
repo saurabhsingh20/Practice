d3ff16b6dbd0b26b68baac759b842246d95b40ff
Saurabh Singh <62989453+saurabhsingh20@users.noreply.github.com>
2022-01-16 18:21:33 +0530

    Add files via upload

---
+++ Bit Magic/Check Kth bit is set or not/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc = new Scanner(System.in);
+        int N = 5;
+        int K = 3;
+        Bit(N,K);
+
+    }
+
+   public static void Bit(int N , int K){
+        if(((N>>(K-1))&1)==1)
+            System.out.println("yes");
+            else
+            System.out.println("no");
+    }
+}
