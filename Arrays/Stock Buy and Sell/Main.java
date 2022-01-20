d92e9225bb1752116e5ad4494152059dda125a48
Saurabh Singh <62989453+saurabhsingh20@users.noreply.github.com>
2022-01-20 15:11:13 +0530

    Add files via upload

---
+++ Arrays/Stock Buy and Sell/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] price = {1,2,3,4,5,6,7,8,14};
+        int n = price.length;
+        System.out.println(MaxProfit(price,n));
+
+    }
+
+    static int MaxProfit(int[] price, int n){
+
+        int profit = 0;
+        for(int i = 1 ; i < n ; i++)
+            if(price[i]>price[i-1])
+                profit += (price[i] -price[i-1]);
+            return profit;
+
+    }
+}
